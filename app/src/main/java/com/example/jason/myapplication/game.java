efb3d30d6a02a02a7b27c3b1917845e7da22348c
jl2569 <jl2569@nau.edu>
2016-10-13 17:07:09 -0700

    gave game submit button a function
    it now cycles through a list of words in the values/strings.xml file in
    the res folder

--- app/src/main/java/com/example/jason/myapplication/game.java
+++ app/src/main/java/com/example/jason/myapplication/game.java
@@ -6,11 +6,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class game extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.active2);
+
+        final TextView GameText = (TextView) findViewById(R.id.textView3) ;
+        final String[] words = getResources().getStringArray(R.array.word);
+        GameText.setText(words[0]);
+        final int[] counter = {1};
+        Button submit = (Button) findViewById(R.id.button4);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GameText.setText(words[counter[0]]);
+                counter[0] +=1;
+
+            }
+        });
 
         Button mainmenu = (Button) findViewById(R.id.button3);
         mainmenu.setOnClickListener(new View.OnClickListener() {
@@ -24,3 +39,4 @@ public class game extends Activity {
     }
 
 }
+
