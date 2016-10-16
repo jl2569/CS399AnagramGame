b55c475ba6293e41cd7025481dc017c074f65f68
Frankie Berry <fmb34@nau.edu>
2016-10-16 22:17:25 +0100

    Added mainmenu button to results screen, somewhat as a test. Also had to change compiler version by 1 to run on my machine; not sure how to avoid that otherwise.

--- app/src/main/java/com/example/jason/myapplication/results.java
+++ app/src/main/java/com/example/jason/myapplication/results.java
@@ -1,7 +1,12 @@
 package com.example.jason.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.Menu;
 
 /**
  * Created by Jason on 10/10/2016.
@@ -12,6 +17,15 @@ public class results extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
 
+        Button mainmenu = (Button) findViewById(R.id.button3);
+        mainmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),MainActivity.class);
+                startActivityForResult(intent,0);
+
+            }
+        });
 
     }
 
