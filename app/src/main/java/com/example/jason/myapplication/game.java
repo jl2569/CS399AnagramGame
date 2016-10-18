da8f2c035f954f3e9184b0c9175ecf86147379e4
jl2569 <jl2569@nau.edu>
2016-10-17 18:22:22 -0700

    fixed almost everything

--- app/src/main/java/com/example/jason/myapplication/game.java
+++ app/src/main/java/com/example/jason/myapplication/game.java
@@ -3,33 +3,70 @@ package com.example.jason.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class game extends Activity {
+    static int score = 0;
+    static String[] answers = new String[5];
+    static int tracker = 1;
+    static int skipcounter = 0;
+    static int counter = 1;
+    static int what = 0 ;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.active2);
-
+        final TextView CounterText = (TextView) findViewById(R.id.textView5) ;
+        final EditText editext = (EditText) findViewById(R.id.editText);
         final TextView GameText = (TextView) findViewById(R.id.textView3) ;
-        final TextView CounterText = (TextView) findViewById(R.id.textView4) ; // meant to hold current number of words completed out of total words (e.g. 4/9)
         final String[] words = getResources().getStringArray(R.array.word);
-        GameText.setText(words[0]);
-        final int[] counter = {1};
+        final String[] skipped = new String[5];
+        final int[] whatskipped = new int[5];
+        final TextView timer = (TextView) findViewById(R.id.textView6) ;
+        new CountDownTimer(30000, 1000) {
 
-        //need to add a timer somewhere
+            public void onTick(long millisUntilFinished) {
+                timer.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                timer.setText("done!");
+            }
+        }.start();
+
+
+
+        CounterText.setText("Question # 1" );
+        GameText.setText(words[0]);
 
         Button submit = (Button) findViewById(R.id.button4);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // maybe check if words array is empty, and if so, then set current word to first word
-                // in new "skip" word array, since we're supposed to let people come back to skipped
-                // words later
-                GameText.setText(words[counter[0]]);
-                counter[0] +=1; // can use the counter to set current number of words completed string, and use length of words to set the total number of words (e.g 4 words completed out of 9 words in total)
+                if(counter != 5) {
+                    answers[tracker-1] = String.valueOf(editext.getText());
+                    tracker +=1;
+                    editext.setText("");
+                    GameText.setText(words[counter]);
+                    counter += 1;
+                    CounterText.setText("Question # " + counter);
+                }else if(skipcounter != 0){
+                    answers[tracker-1] = String.valueOf(editext.getText());
+                    tracker-=1;
+                    editext.setText("");
+                    GameText.setText(skipped[skipcounter-1]);
+                    skipcounter-=1;
+                }else{
+                    answers[tracker-1] = String.valueOf(editext.getText());
+                    counter = 1;
+                    tracker = 1;
+                    skipcounter = 0;
+                    Intent intent = new Intent(v.getContext(),results.class);
+                    startActivityForResult(intent,0);
+                }
 
             }
         });
@@ -38,9 +75,15 @@ public class game extends Activity {
         skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // currently empty but potentially do the same as submit, but don't record the answer
-                // and instead store the skipped word in a skipped words array to be displayed later
-
+                if(counter != 5) {
+                    skipped[skipcounter] = words[counter-1];
+                    tracker+=1;
+                    skipcounter +=1;
+                    editext.setText("");
+                    CounterText.setText("Question # 1" );
+                    GameText.setText(words[counter]);
+                    counter += 1;
+                }
             }
         });
 
@@ -48,6 +91,10 @@ public class game extends Activity {
         mainmenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                counter = 1;
+                tracker = 1;
+                answers = new String[5];
+                skipcounter = 0;
                 Intent intent = new Intent(v.getContext(),MainActivity.class);
                 startActivityForResult(intent,0);
 
@@ -56,4 +103,3 @@ public class game extends Activity {
     }
 
 }
-
--- app/src/main/java/com/example/jason/myapplication/results.java
+++ app/src/main/java/com/example/jason/myapplication/results.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.Menu;
+
+import static com.example.jason.myapplication.game.answers;
+import static com.example.jason.myapplication.game.score;
 
 /**
  * Created by Jason on 10/10/2016.
@@ -16,21 +18,40 @@ public class results extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
+        final TextView GameText = (TextView) findViewById(R.id.textView2) ;
+        final TextView result = (TextView) findViewById(R.id.textView4) ;
+        final String[] com = getResources().getStringArray(R.array.answers);
+        final String[] questions = getResources().getStringArray(R.array.word);
+        final String[] answers = game.answers;
 
-        // if we have stored answers here, we can iterate through them and display each one as a block since the results screen
-        // is supposed to display the user's answers I think. Can also iterate through words array to display the word given
-        // if we have a score stored as well we'd probably use that too
-
+        result.setText("Question # 1: " + questions[0] +" Answer # 1: " + answers[0]+ compare(answers[0],com[0]) +"\n" +
+                "Question # 2: " + questions[1] +" Answer # 2: " + answers[1]+compare(answers[1],com[1]) +"\n" +
+                "Question # 3: " + questions[2] +" Answer # 3: " + answers[2]+compare(answers[2],com[2]) +"\n" +
+                "Question # 4: " + questions[3] +" Answer # 4: " + answers[3]+compare(answers[3],com[3]) +"\n" +
+                "Question # 5: " + questions[4] +" Answer # 5: " + answers[4]+compare(answers[4],com[4]) +"\n" );
+        GameText.setText(score + "/5" );
         Button mainmenu = (Button) findViewById(R.id.button3);
         mainmenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                score = 0;
+                game.answers = null;
                 Intent intent = new Intent(v.getContext(),MainActivity.class);
                 startActivityForResult(intent,0);
 
             }
         });
 
+
+    }
+
+    public String compare(String a, String b) {
+        if(a.equals(b)){
+            score +=1;
+            return " Correct";
+        }else{
+            return " Wrong";
+        }
     }
 
 }
